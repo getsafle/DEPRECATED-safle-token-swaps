ee2ea85e7ea24c5ddeadf63eacb426c9a12c36d9
Shubham Singhal <aggshubh95@gmail.com>
2021-01-29 18:48:23 +0530

    kyber proxy contract variable usage updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,25 +1,12 @@
 module.exports = {
-  kyberProxyContractAddress: '0x818e6fecd516ecc3849daf6845e3ec868087b755',
-  KYBER_CURRENCY_URL:
-    process.env.KYBER_CURRENCY_URL ||
-    'https://api.kyber.network/currencies',
-  KYBER_GET_GAS_LIMIT_URL:
-    process.env.KYBER_GET_GAS_LIMIT_URL ||
-    'https://api.kyber.network/gas_limit',
-  REF_ADDRESS:
-    process.env.REF_ADDRESS || '0x0000000000000000000000000000000000000000',
-  ETH_TOKEN_ADDRESS:
-    process.env.ETH_TOKEN_ADDRESS ||
-    '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
-  MAX_ALLOWANCE:
-    process.env.MAX_ALLOWANCE ||
-    '115792089237316195423570985008687907853269984665640564039457584007913129639935',
+  KYBER_PROXY_CONTRACT_ADDRESS: '0x818E6FECD516Ecc3849DAf6845e3EC868087B755',
+  KYBER_CURRENCY_URL: 'https://api.kyber.network/currencies',
+  KYBER_GET_GAS_LIMIT_URL: 'https://api.kyber.network/gas_limit',
+  REF_ADDRESS: '0x0000000000000000000000000000000000000000',
+  ETH_TOKEN_ADDRESS: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+  MAX_ALLOWANCE: '115792089237316195423570985008687907853269984665640564039457584007913129639935',
   ETHERSCAN_TRANSACTION_URL: 'https://etherscan.io/tx',
-  KYBER_SWAP_TOKEN_IMAGE_BASE_URL:
-    'https://files.kyberswap.com/DesignAssets/tokens/',
-  ETHERSCAN_SERVICE_URL:
-    process.env.ETHERSCAN_SERVICE_URL || 'https://api.etherscan.io/api',
-  ETHERSCAN_SECRET:
-    process.env.ETHERSCAN_SECRET || 'C2VBJIH1PWRNFDA2ZT1P3W26NGESGCBUAN',
-
+  KYBER_SWAP_TOKEN_IMAGE_BASE_URL: 'https://files.kyberswap.com/DesignAssets/tokens/',
+  ETHERSCAN_SERVICE_URL: 'https://api.etherscan.io/api',
+  ETHERSCAN_SECRET: 'C2VBJIH1PWRNFDA2ZT1P3W26NGESGCBUAN',
 };
