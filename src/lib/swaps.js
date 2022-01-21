82fa89ae7166baa7bd86a6dba13975dba93c7fe5
shubham <shubham@getsafle.com>
2022-01-21 18:25:33 +0530

    Added method to get list of supported tokens

--- src/lib/swaps.js
+++ src/lib/swaps.js
@@ -1,5 +1,7 @@
 const  supportedDex  = require('../dex');
-const response = require('../constants/responses')
+const response = require('../constants/responses');
+const config = require('../config');
+const helper = require('../utils/helper')
 
 class Swaps {
 
@@ -19,6 +21,12 @@ class Swaps {
         }
         this.dex = dex;
     }    
+
+    async getSupportedTokens() {
+        const { response } = await helper.getRequest({ url: config.SUPPORTED_TOKENS_URL });
+    
+        return response;
+    }
 }
 
 module.exports = Swaps;
